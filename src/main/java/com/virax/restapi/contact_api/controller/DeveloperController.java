f769d861af12b60e33dba393cbb88cc92eeb2858
ganesh714 <evvganesh1@gmail.com>
2026-06-06 13:00:20 +0530

    created gen token method

--- src/main/java/com/virax/restapi/contact_api/controller/DeveloperController.java
+++ src/main/java/com/virax/restapi/contact_api/controller/DeveloperController.java
@@ -8,36 +8,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*; // Consolidated imports
 
 import com.virax.restapi.contact_api.dtos.DeveloperDto;
+import com.virax.restapi.contact_api.dtos.TokenUserDto;
 import com.virax.restapi.contact_api.service.DeveloperService;
 
 @RestController
-@RequestMapping("/api/developers")
 public class DeveloperController {
 
     @Autowired
     private DeveloperService developerService;
+    
+    @PostMapping(value = {"/generateToken","/user/login"})
+    public ResponseEntity<String> generateToken(@RequestBody TokenUserDto tokenUserDto){
+    	return new ResponseEntity<>(developerService.generateToken(tokenUserDto),HttpStatus.OK);
+    }
 
-    @PostMapping("/add")
+    @PostMapping("/api/developers/add")
     public ResponseEntity<DeveloperDto> addDeveloper(@RequestBody DeveloperDto developerDto) {
         return new ResponseEntity<>(developerService.addDeveloper(developerDto), HttpStatus.CREATED);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/api/developers/update/{id}")
     public ResponseEntity<DeveloperDto> updateDeveloper(@PathVariable int id, @RequestBody DeveloperDto developerDto) {
         return new ResponseEntity<>(developerService.updateDeveloper(id, developerDto), HttpStatus.OK);
     }
 
-    @GetMapping("/all")
+    @GetMapping("/api/developers/all")
     public ResponseEntity<List<DeveloperDto>> getAllDevelopers() {
         return new ResponseEntity<>(developerService.getAllDevelopers(), HttpStatus.OK);
     }
 
-    @GetMapping("/get/{userName}")
+    @GetMapping("/api/developers/get/{userName}")
     public ResponseEntity<DeveloperDto> getDeveloperByUserName(@PathVariable String userName) {
         return new ResponseEntity<>(developerService.getDeveloperByUserName(userName), HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/api/developers/delete/{id}")
     public ResponseEntity<String> deleteDeveloper(@PathVariable int id) {
         developerService.deleteDeveloper(id);
         return new ResponseEntity<>("Developer deleted successfully! 🎉", HttpStatus.OK);
