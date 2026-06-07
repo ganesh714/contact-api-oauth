3686af5b98e25eec601e50e5fa8465d47ff8c101
ganesh714 <evvganesh1@gmail.com>
2026-06-07 09:50:43 +0530

    updated jwtservice with some methods

--- src/main/java/com/virax/restapi/contact_api/service/JwtService.java
+++ src/main/java/com/virax/restapi/contact_api/service/JwtService.java
@@ -5,6 +5,7 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -12,8 +13,10 @@ import javax.crypto.SecretKey;
 import org.springframework.stereotype.Service;
 
 import com.virax.restapi.contact_api.model.Developer;
+import com.virax.restapi.contact_api.model.DeveloperUserDetails;
 import com.virax.restapi.contact_api.model.Role;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -50,5 +53,33 @@ public class JwtService {
 	public SecretKey getKey() {
 		byte[] bytesKey = Base64.getDecoder().decode(secretKey);
 		return Keys.hmacShaKeyFor(bytesKey);
+	}
+	
+	public <T> T extractClaims(String jwtToken, Function<Claims, T> typeOfClaim) {
+		
+		Claims claims = Jwts.parser()
+				.verifyWith(getKey())
+				.build()
+				.parseSignedClaims(jwtToken)
+				.getPayload();
+		return typeOfClaim.apply(claims);
+	}
+	
+	public String extractUserName(String jwtToken) {
+		return extractClaims(jwtToken, Claims::getSubject);
+	}
+	
+	public boolean isTokenExpired(String jwtToken) {
+		Date expireDate = extractClaims(jwtToken, Claims::getExpiration);
+		return expireDate.before(new Date());
+	}
+	
+	public boolean validateToken(String jwtToken, DeveloperUserDetails userDetails) {
+		String userName = extractUserName(jwtToken);
+		
+		if (userName.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken)) {
+			return true;
+		}
+		return false;
 	}
 }
