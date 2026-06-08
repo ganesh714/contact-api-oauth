40117bc59db9524b4d996b4799d474d7d2d23fce
ganesh714 <evvganesh1@gmail.com>
2026-06-08 14:01:22 +0530

    ========================= completed RMA jwt ======================================

--- src/main/java/com/virax/restapi/contact_api/service/JwtService.java
+++ src/main/java/com/virax/restapi/contact_api/service/JwtService.java
@@ -1,6 +1,11 @@
 package com.virax.restapi.contact_api.service;
 
+import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
@@ -10,7 +15,12 @@ import java.util.function.Function;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.virax.restapi.contact_api.model.Developer;
 import com.virax.restapi.contact_api.model.DeveloperUserDetails;
@@ -23,12 +33,59 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 	
-	private String secretKey = "";
 	
-	public JwtService() throws NoSuchAlgorithmException {
-		KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
-		SecretKey secretKey = keyGenerator.generateKey();
-		this.secretKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+	@Value("${private.key.path}")
+	private String privateKeyFilePathKey;
+	
+	@Value("${public.key.path}")	
+	private String publicKeyFilePathKey;
+	
+	
+	public PrivateKey loadPrivateKey(String privateKeyPath) {
+		try {
+			Resource resource = new ClassPathResource(privateKeyPath);
+			
+			String key= new String(resource.getInputStream().readAllBytes());
+			
+			key =  key.replace("-----BEGIN PRIVATE KEY-----", "")
+				.replace("-----END PRIVATE KEY-----", "")
+				.replaceAll("\\s+", "");
+			
+			byte[] keyBytes = Base64.getDecoder().decode(key);
+			
+			PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+			
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			
+			return keyFactory.generatePrivate(keySpec);
+		} catch (Exception e) {
+			throw new RuntimeException("unable to load Pri K", e);
+		}
+	}
+	
+	public PublicKey loadPublicKey(String publicKey) {
+		
+		try {
+			
+			Resource resource = new ClassPathResource(publicKey);
+			
+			String key = new String(resource.getInputStream().readAllBytes());
+			
+			key = key.replace("-----BEGIN PUBLIC KEY-----", "")
+				.replace("-----END PUBLIC KEY-----", "")
+				.replaceAll("\\s+", "");
+			
+			byte[] keyBytes = Base64.getDecoder().decode(key);
+			
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+			
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			
+			return keyFactory.generatePublic(keySpec);
+				
+		} catch (Exception e) {
+			throw new RuntimeException("cant load Pub K", e);
+		}
 	}
 	
 	public String generateToken(Developer user) {
@@ -45,20 +102,16 @@ public class JwtService {
 				.subject(user.getUserName())
 				.issuedAt(new Date(System.currentTimeMillis()))
 				.expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-				.signWith(getKey())
+				.signWith(loadPrivateKey(privateKeyFilePathKey))
 				.compact();
 		
 	}
-	
-	public SecretKey getKey() {
-		byte[] bytesKey = Base64.getDecoder().decode(secretKey);
-		return Keys.hmacShaKeyFor(bytesKey);
-	}
+
 	
 	public <T> T extractClaims(String jwtToken, Function<Claims, T> typeOfClaim) {
 		
 		Claims claims = Jwts.parser()
-				.verifyWith(getKey())
+				.verifyWith(loadPublicKey(publicKeyFilePathKey))
 				.build()
 				.parseSignedClaims(jwtToken)
 				.getPayload();
